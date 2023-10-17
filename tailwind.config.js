a6ff2b2db1a16c562554b698a1bba255fddd4083
thechewgum <quentin.brandy@protonmail.com>
2023-10-17 14:52:19 +0200

    base css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "base-white": "hsl(var(--base--white))",
+        "base-black": "hsl(var(--base--black))",
+       "clr-light-grey": "hsl(var(--colors--light-grey))",
+       "clr-grey": "hsl(var(--colors--grey))",
+       "clr-dark-grey": "hsl(var(--colors--dark-grey))",
+       "clr-light-green": "hsl(var(--colors--light-green))",
+       "clr-green": "hsl(var(--colors--green))",
+       "clr-cons-xbox": "hsl(var(--consoles--green-xbox))",
+       "clr-cons-ps": "hsl(var--consoles--blue-ps))",
+       "clr-cons-nintendo": "hsl(var(--consoles--red-nintendo))",
+    },
+    fontFamily:{
+      'inter': ["'Inter', sans-serif"],
+      
+  },
+    },
   },
   plugins: [],
 }
